c1e7f58b9da14869c9757413689318770e6474df
codebytes111 <codebytes111@gmail.com>
2025-09-16 18:14:36 +0530

    'commiting changes'

---
+++ javScri/chapters/index.js
@@ -0,0 +1,5 @@
+console.log('im inside the index.js');
+console.warn('warn give i');
+console.error('this dff');
+//console.clear();  /*HI HELLO*/
+/*thid id */
---
+++ javScri/numbers&strings/add.js
@@ -0,0 +1,31 @@
+// At a restaurant you ate: 1 Dal₹ 100, 2 Roti ₹10 each, 1 Ice Cream 30, calculate and display the final bill amount.
+
+(1*100)+(2*10)+(1*30)
+150
+
+'$'+(1*100)+(2*10)+(1*30)
+'$1002030'
+
+'$'+((1*100)+(2*10)+(1*30))
+'$150'
+
+`your final bill is: $${((1*100)+(2*10)+(1*30))}`
+'your final bill is: $150'
+
+// 2. Calculate 18% GST on iPhone 15 ₹79,990 and 2
+// Air pods Pro ₹24990 each.
+
+
+
+// 3. Create strings using all 3 methods.
+
+
+
+// 4. Concatenate String with Strings, and String with numbers.
+
+
+// 5. Create Order Summary String for our Myntra Cart.
+
+
+// 6. Display order summary in a popup
+
